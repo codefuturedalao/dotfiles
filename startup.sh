5e69fcbb9cb5a8e184fea484d4a6ad9f5ca0d864
hacksang <qlsang@whu.edu.cn>
2023-04-09 11:24:05 +0800

    add note about need for complie ycm

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 git clone https;//github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#please remember the YouCompleteme Plugin need to be compiled
